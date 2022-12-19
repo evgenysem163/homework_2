95e86eb2bb66553a3083b3bea9d88e936c0b842e
evgeny_sem163 <evg82tol@gmail.com>
2022-12-19 20:55:33 +0300

    переменой var присваиваем три разных значения. результат выводим в консоль

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        /* Для решения этой задачи используеь  тип переменной var, присваиваем переменной var три разных значения. посвле этого выводим значение каждой переменной в консоль*/
+   var dog = 8.0;
+   System.out.println(dog);
+   var cat = 3.6;
+   System.out.println(cat);
+   var paper = 763789;
+   System.out.println(paper);
+    }
+
+}
