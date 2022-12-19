6c6d2433f45f7558abcd2fd0327c72073915d342
evgeny_sem163 <evg82tol@gmail.com>
2022-12-19 22:01:40 +0300

    переменой var присваиваем три разных значения. результат выводим в консоль. 2) далее присваиваем каждому значение +4. 3) уменьшение значений на значения указаные в задании.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,28 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         /* Для решения этой задачи используеь  тип переменной var, присваиваем переменной var три разных значения. посвле этого выводим значение каждой переменной в консоль*/
    var dog = 8.0;
-   System.out.println(dog);
-   dog = dog + 4;
-   System.out.println(dog);
    var cat = 3.6;
-   System.out.println(cat);
-   cat = cat + 4;
-   System.out.println(cat);
    var paper = 763789;
-   System.out.println(paper);
-   paper = paper + 4;
-   System.out.println(paper);
+       System.out.println(dog);
+       System.out.println(cat);
+       System.out.println(paper);
+       dog = dog + 4;
+       cat = cat + 4;
+       paper = paper +4;
+       System.out.println(dog);
+       System.out.println(cat);
+       System.out.println(paper);
+       dog = dog -3.5;
+       cat = cat - 1.6;
+       paper = paper - 7639;
+       System.out.println(dog);
+       System.out.println(cat);
+       System.out.println(paper);
+
+
     }
 
 }
