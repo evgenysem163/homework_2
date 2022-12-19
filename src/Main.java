9e3b1f2db147c73cea899229cae0f9dbe74e874b
evgeny_sem163 <evg82tol@gmail.com>
2022-12-19 21:10:11 +0300

    переменой var присваиваем три разных значения. результат выводим в консоль. 2) далее присваиваем каждому значение +4.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,15 @@ public class Main {
         /* Для решения этой задачи используеь  тип переменной var, присваиваем переменной var три разных значения. посвле этого выводим значение каждой переменной в консоль*/
    var dog = 8.0;
    System.out.println(dog);
+   dog = dog + 4;
+   System.out.println(dog);
    var cat = 3.6;
    System.out.println(cat);
+   cat = cat + 4;
+   System.out.println(cat);
    var paper = 763789;
+   System.out.println(paper);
+   paper = paper + 4;
    System.out.println(paper);
     }
 
